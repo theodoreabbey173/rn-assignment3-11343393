52f44f9b84f42f751c165a03c9059b1baf236b35
theodoreabbey173 <theodoreabbey173@gmail.com>
2024-06-02 13:27:36 +0000

    Created my javascript  files to begin work

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Greeting from './components/Greeting';
+import SearchBar from './components/SearchBar';
+import Categories from './components/Categories';
+import OngoingTask from './components/OngoingTask';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Greeting />
+      <ScrollView>
+      <View style={styles.searchContainer}>
+        <View style={styles.search}>
+        <SearchBar  />
+        </View>
+      <View style={styles.settings}>
+      <Image style={styles.settingsImage} source={require('./images/settings.jpeg')}  />
+      </View>
+      </View>
+      <Categories />
+      <OngoingTask />
       <StatusBar style="auto" />
+    
+    </ScrollView>
     </View>
   );
 }
@@ -13,8 +31,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f7f0e8',
+    
   },
+  searchContainer:{
+    display:'flex',
+    flexDirection:'row',
+    columnGap:15,
+  }
+  ,search:{
+    flex:4
+  },
+  settings:{
+    flex:1,
+    borderRadius:10
+  },
+  settingsImage:{
+    resizeMode:'contain',
+    height:50,
+    marginTop:30,
+    borderRadius:10,
+    marginLeft: -30
+   // marginRight:1
+    
+  }
 });
+ 
